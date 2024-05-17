d584d0ab55dfafcedb5f0b0fca4c7ed9de10c1f0
Niranthari <nirantharishurthis@gmail.com>
2024-05-17 23:32:19 +0530

    status

--- chess/frontend/src/App.tsx
+++ chess/frontend/src/App.tsx
@@ -9,12 +9,14 @@ function App() {
   //const [count, setCount] = useState(0)
 
   return (
+    <div className='h-screen bg-slate-950'>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Landing/>}></Route>
       <Route path="/game" element={<Game/>}></Route>
     </Routes>
   </BrowserRouter>
+  </div> 
   )
 }
 
--- chess/frontend/src/screens/Landing.tsx
+++ chess/frontend/src/screens/Landing.tsx
@@ -2,10 +2,10 @@ import React from "react"
 
 export const Landing = ()=>{
     return <div>
-       <div className = "mt-2">
+       <div className = "pt-8">
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                <div>
-                    <img src={"/chessboard.png"}/>
+                <div className="flex justify-center">
+                    <img src={"/chessboard.png"} className="max-w-96"/>
                 </div>
                 <div>
                     <h1 className="text-4xl font bold"> 
